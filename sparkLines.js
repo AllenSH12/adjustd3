c0c2e7cc6cae1d18d7637035f9787383903e609e
Allen Sogis-Hernandez <sogishernandez.allen@gmail.com>
2013-03-05 20:58:46 -0600

    attempted changing sparkLines to a function, didn't work

--- sparkLines.js
+++ sparkLines.js
@@ -1,27 +1,30 @@
-function addSparkLine(location, importedData) {
-	var margin = {top: 10, right: 10, bottom: 15, left: 10},
-	width = 265 - margin.left - margin.right,
-	height = 100 - margin.top - margin.bottom;
+function getSparkLine(dataFile, location) {
+	var margin = {top: 5, right: 5, bottom: 5, left: 5},
+		width = 265 - margin.left - margin.right,
+		height = 100 - margin.top - margin.bottom;
 
 	var parseDate = d3.time.format("%d-%b-%y").parse;
 
 	var x = d3.time.scale()
-	.range([0, width]);
+		.range([0, width]);
 
 	var y = d3.scale.linear()
-	.range([height, 0]);
+		.range([height, 0]);
 
-	var line = d3.svg.line()
-	.x(function(d) { return x(d.date); })
-	.y(function(d) { return y(d.close); });
+	var sparkLine = d3.svg.line()
+		.interpolate("basis")
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y(d.close); });
 
-	var svg = d3.select(document.getElementById(location)).append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	sparkSVG = d3.select(location)
+		.style('width', "" + width + "px")
+		.append('sparkSVG:svg')
+		.attr("width", width)
+		.attr("height", height)
+		.attr('class', 'sparkLine');
+		
 
-	d3.tsv(importedData, function(error, data) {
+	d3.tsv(dataFile, function(error, data) {
 		data.forEach(function(d) {
 		d.date = parseDate(d.date);
 		d.close = +d.close;
@@ -30,17 +33,39 @@ function addSparkLine(location, importedData) {
 	x.domain(d3.extent(data, function(d) { return d.date; }));
 	y.domain(d3.extent(data, function(d) { return d.close; }));
 
-	svg.append("path")
-	.datum(data)
-	.attr("class", "line")
-	.attr("d", line);
+	sparkSVG.append("path")
+		.datum(data)
+		.attr("class", "sparkLine")
+		.attr("d", sparkLine);
 	});
 
 	var aspect = 265 / 100,
-	chart = $("#sparkOne");
+	chart = $("#sparkSVG");
 	$(window).on("resize", function() {
 		var targetWidth = chart.parent().width();
 		chart.attr("width", targetWidth);
 		chart.attr("height", targetWidth / aspect);
-	});	
+	});
+	return false;
+}
+
+function updateData(dataFile, variableToUpdate) {
+	d3.tsv(dataFile, function(error, data) {
+		data.forEach(function(d) {
+			d.date = parseDate(d.date);
+			d.close = +d.close;
+		})
+		for (var i=8; i < data.length; i++) {
+			data[i].close = data[i].close * (1 + variableToUpdate);
+		}
+
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+	  	y.domain(d3.extent(data, function(d) { return d.close; }));
+
+		sparkSVG.selectAll("path")
+			.data([data])
+			.transition().duration(2000).delay(200)
+			.attr("d",sparkLine);
+	});
+	return false;
 }
