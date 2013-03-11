83ec58ed0af6a6e96d837e3dc293e405ee03a31d
Allen <sogishernandez.allen@gmail.com>
2013-03-11 11:29:00 -0500

    fixed conflicts

--- getLineGraph.js
+++ getLineGraph.js
@@ -2,7 +2,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
   width = 1140 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
-var max = 0;
+var parseDate = d3.time.format("%d-%b-%y").parse;
 
 var x = d3.time.scale()
   .range([0, width])
@@ -24,7 +24,6 @@ var line = d3.svg.line()
   .interpolate("basis")
   .x(function(d) { return x(d.date); })
   .y(function(d) { return y(d.close); });
-<<<<<<< HEAD
 
 var upsideLine = d3.svg.line()
   .interpolate("basis")
@@ -41,8 +40,6 @@ var area = d3.svg.area()
 	.x(function(d) { return x(d.date); })
   .y0(height)
 	.y1(function(d) { return y(d.close); });
-=======
->>>>>>> changes
 
 svg = d3.select('#chart')
   	.append('svg:svg')
@@ -52,8 +49,6 @@ svg = d3.select('#chart')
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("data/yelpSharePrices.csv", function(error, data) {
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
 	data.forEach(function(d) {
 		d.date = parseDate(d.date);
 		d.close = +d.close;
@@ -61,17 +56,7 @@ d3.csv("data/yelpSharePrices.csv", function(error, data) {
     d.downside = +d.downside;
 	})
 
-  var priceTargets = color.domain().map(function(name) {
-    return {
-      name: name,
-      values: data.map(function(d) {
-        return {data: d.date, close: +d[name]};
-      })
-    };
-  })
-
 	x.domain(d3.extent(data, function(d) { return d.date; }));
-<<<<<<< HEAD
 	y.domain([0,d3.max(data, function(d) { return d.upside; })]);
 
     svg.selectAll(".area")
@@ -134,42 +119,6 @@ d3.csv("data/yelpSharePrices.csv", function(error, data) {
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .text("Price ($)");
-=======
-  y.domain([
-    d3.min(priceTargets, function(c) { return d3.min(c.values, function(v) { return v.close; }); }),
-    d3.max(priceTargets, function(c) { return d3.max(c.values, function(v) { return v.close; }); })
-  ]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Close ($)");
-
-  var priceTargets = svg.selectAll(".priceTarget")
-      .data(priceTargets)
-    .enter().append("g")
-      .attr("class", "priceTarget");
-
-  priceTargets.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.name); });
-
-  priceTargets.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.close) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
->>>>>>> changes
 });
